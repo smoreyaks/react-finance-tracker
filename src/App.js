477da0f81c11ddec41f812d53ed75dc1041606a4
smoreyaks <bnixon.eng@gmail.com>
2022-08-30 11:44:46 +1000

    Create Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+// Elements
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+// Components
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+
+// Styles
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/login">
+            <Login/>
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
