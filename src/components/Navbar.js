f1c0541ebcc06488e1352e6bee7e5645c746d4d3
smoreyaks <bnixon.eng@gmail.com>
2022-09-04 23:31:30 +1000

    Firebase Auth State Changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar() {
                 )}
                 { user && (
                     <>
-                        <li>{user.displayName}</li>
+                        <li>Welcome, {user.displayName}</li>
                         <li>
                             <button 
                                 className='btn'
