79efc8f1e60cc50236668271f8f3a1d0b8a06b9a
smoreyaks <bnixon.eng@gmail.com>
2022-09-06 15:43:59 +1000

    Ordering Firestore Queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -2,11 +2,12 @@
 import { useState, useEffect, useRef } from "react"
 import { projectFirestore } from "../firebase/Config"
 
-export const useCollection = (collection, _query ) => {
+export const useCollection = (collection, _query, _orderBy ) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
 
     const query = useRef(_query).current
+    const orderBy = useRef(_orderBy).current
 
     useEffect(() => {
         let ref = projectFirestore.collection( collection )
@@ -15,6 +16,9 @@ export const useCollection = (collection, _query ) => {
         // _query is an array and is "different" on every function call
         if (query) {
             ref = ref.where(...query)
+        }
+        if (orderBy) {
+            ref = ref.orderBy(...orderBy)
         }
 
         const unsubscribe = ref.onSnapshot(snapshot => { 
@@ -34,7 +38,7 @@ export const useCollection = (collection, _query ) => {
         // Unsubscribe on mount
         return () => unsubscribe()
 
-    }, [collection, query])
+    }, [collection, query, orderBy])
 
     return { documents, error}
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,8 +14,8 @@ export default function Home() {
     const { user } = useAuthContext()
     const { documents, error } = useCollection(
         'transactions',
-        ["uid", "==", user.uid]
-        
+        ["uid", "==", user.uid],
+        ["createdAt", "desc"]
         )
 
     return (
