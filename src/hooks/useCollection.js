e03e95a7600f570fc7f413f2bef8bc8e090b6276
smoreyaks <bnixon.eng@gmail.com>
2022-09-06 15:21:20 +1000

    Firestore Query

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,13 +1,21 @@
 // Hooks
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { projectFirestore } from "../firebase/Config"
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query ) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
 
+    const query = useRef(_query).current
+
     useEffect(() => {
-        let ref = projectFirestore.collection(collection)
+        let ref = projectFirestore.collection( collection )
+
+        // If we don't use a reference, an infinite loop in useEffect will occur
+        // _query is an array and is "different" on every function call
+        if (query) {
+            ref = ref.where(...query)
+        }
 
         const unsubscribe = ref.onSnapshot(snapshot => { 
             let results = [];
@@ -26,7 +34,7 @@ export const useCollection = (collection) => {
         // Unsubscribe on mount
         return () => unsubscribe()
 
-    }, [collection])
+    }, [collection, query])
 
     return { documents, error}
 }
